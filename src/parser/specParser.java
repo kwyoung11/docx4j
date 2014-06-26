ed9d4dd3eed627ffee7dab294aaf37a241dbd2ba
Kevin Young <kyoung18@terpmail.umd.edu>
2014-06-26 18:42:10 -0400

    initial commit

---
+++ src/parser/specParser.java
@@ -0,0 +1,23 @@
+package parser;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.extractor.ExtractorFactory;
+
+public class specParser {
+
+	 public static void main(String[] args) throws Exception {   
+		 System.out.println("hello");
+	 }  
+}   
+
+
